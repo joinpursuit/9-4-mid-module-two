b5fb4e41ef3d2c9069b465c7be45d8147e7c4e8b
AdrianBurke1 <adrianburke902@gmail.com>
2022-11-28 18:58:05 -0500

    “9-4-mid-module-two”

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ const exampleMovies = require("./movies");
       "James and the Giant Peach",
     ];
  */
-function getAllMovieTitles() {}
+function getAllMovieTitles(movies) {
+  if (movies.length === 0) {
+    throw ('Error')
+  }
+  return movies.map(movie => movie.title)
+}
 
 /**
  * checkIfAnyMovieHasRating()
